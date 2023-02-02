63ea953de912a0b34e073b6e75155c57b4f167ba
Steven Nguyen <omgsteven@gmail.com>
2023-02-01 19:33:50 -0500

    Adding new project

---
+++ app.js
@@ -0,0 +1,59 @@
+const grid = document.querySelector('.grid');
+let gridValue = document.querySelector('.grid-size');
+let gridSize = document.querySelector('input');
+const resetBtn = document.querySelector('.reset');
+const applyGridSize = document.querySelector('.apply')
+let squareSize = 8;
+
+createGrid(squareSize)
+
+// Create Squared Divs
+
+function createDiv(size){
+    const div = document.createElement('div');
+    div.classList.add('box');
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+
+    return div;
+}
+
+//create grid and append to grid
+
+function createGrid(gridSize){
+    for(let i = 0; i < gridSize; i++){
+        for(let j = 0; j < gridSize; j++){
+            grid.appendChild(createDiv(grid.clientWidth / gridSize));
+        }
+    }
+}
+
+// Reset Grid
+
+function reset(){
+    while(grid.firstChild){
+        grid.removeChild(grid.lastChild);
+    }
+
+    createGrid(squareSize);
+}
+
+// Event delgation to target children of grid
+
+grid.addEventListener('mouseover', function (e) {
+    // Add the "active" class to only divs with a "box" class
+    if (e.target.matches('.box')) {
+      e.target.classList.add('active');
+    }
+  });
+  
+  gridSize.addEventListener('input', function (e) {
+    squareSize = e.target.value;
+    gridValue.textContent = `${squareSize}x${squareSize}`;
+  });
+  
+  applyGridSize.addEventListener('click', function () {
+    reset();
+  });
+  
+  resetBtn.addEventListener('click', reset);
